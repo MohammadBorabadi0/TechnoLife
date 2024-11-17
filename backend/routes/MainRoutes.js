ae3778798b278458eb32c92305d0d8c73cdb5d1a
mohammadborabadi0.com <mohammadborabadi0@gmail.com>
2024-11-17 21:43:07 +0330

    feat(backend): add category routes to main routes

--- backend/routes/MainRoutes.js
+++ backend/routes/MainRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 // routes
 import userRoutes from "./UserRoutes.js";
 import productRoutes from "./ProductRoutes.js";
+import categoryRoutes from "./CategoryRoutes.js";
 
 const router = express.Router();
 
 // Mount the category and user routes
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
+router.use("/categories", categoryRoutes);
 
 export default router;
