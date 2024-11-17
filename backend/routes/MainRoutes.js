5da8b7c98472a9183171810aafb16421efef76ff
mohammadborabadi0.com <mohammadborabadi0@gmail.com>
2024-11-17 21:44:36 +0330

    feat(backend): add color routes to main routes

--- backend/routes/MainRoutes.js
+++ backend/routes/MainRoutes.js
@@ -4,13 +4,15 @@ import userRoutes from "./UserRoutes.js";
 import productRoutes from "./ProductRoutes.js";
 import categoryRoutes from "./CategoryRoutes.js";
 import brandRoutes from "./BrandRoutes.js";
+import colorRoutes from "./ColorRoutes.js";
 
 const router = express.Router();
 
 // Mount the category and user routes
 router.use("/users", userRoutes);
+router.use("/colors", colorRoutes);
+router.use("/brands", brandRoutes);
 router.use("/products", productRoutes);
 router.use("/categories", categoryRoutes);
-router.use("/brands", brandRoutes);
 
 export default router;
