61cdc22844935e9b4296c33f05861cbd84aaa380
mohammadborabadi0.com <mohammadborabadi0@gmail.com>
2024-11-17 21:27:09 +0330

    feat(backend): add user model

---
+++ backend/models/User.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+        googleId: {
+            type: String,
+            required: false,
+        },
+        avatar: {
+            type: String,
+            required: false,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: false,
+        },
+        isAdmin: { type: Boolean, required: true, default: false },
+        mobile: {
+            type: String,
+        },
+        cardNumber: {
+            type: String,
+        },
+        nationalCode: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Encrypt password using bcrypt
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
