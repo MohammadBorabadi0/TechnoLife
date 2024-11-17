ebe598d2adcd87f06c64ee9d66c9184084b655b0
mohammadborabadi0.com <mohammadborabadi0@gmail.com>
2024-11-17 21:27:47 +0330

    feat(backend): add user routes

---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import {
+    createUser,
+    deleteUser,
+    getAllUsers,
+    getUserById,
+    getUserProfile,
+    loginUser,
+    logout,
+    updateUser,
+    updateUserProfile,
+} from "../controllers/UserController.js";
+import { protect } from "../middlewares/authMiddleware.js";
+
+const router = Router();
+
+router.route("/users").get(protect, getAllUsers);
+
+router.route("/user/profile").get(protect, getUserProfile);
+router.route("/user/profile/:id").put(protect, updateUserProfile);
+
+router
+    .route("/user/:id")
+    .get(protect, getUserById)
+    .put(protect, updateUser)
+    .delete(protect, deleteUser);
+
+router.route("/register").post(createUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(protect, logout);
+
+export default router;
