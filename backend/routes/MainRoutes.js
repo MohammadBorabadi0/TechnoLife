de1c04c9e4ce22ec76537f12eb33bfc6986dbb81
mohammadborabadi0.com <mohammadborabadi0@gmail.com>
2024-11-17 21:45:34 +0330

    feat(backend): add order routes to main routes

--- backend/routes/MainRoutes.js
+++ backend/routes/MainRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 // routes
 import userRoutes from "./UserRoutes.js";
-import productRoutes from "./ProductRoutes.js";
-import categoryRoutes from "./CategoryRoutes.js";
+import orderRoutes from "./OrderRoutes.js";
 import brandRoutes from "./BrandRoutes.js";
 import colorRoutes from "./ColorRoutes.js";
+import productRoutes from "./ProductRoutes.js";
+import categoryRoutes from "./CategoryRoutes.js";
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ const router = express.Router();
 router.use("/users", userRoutes);
 router.use("/colors", colorRoutes);
 router.use("/brands", brandRoutes);
+router.use("/orders", orderRoutes);
 router.use("/products", productRoutes);
 router.use("/categories", categoryRoutes);
 
