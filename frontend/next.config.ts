df97cc6674c02b6c13b30aac7704b206fd99e5f7
mohammadborabadi0.com <mohammadborabadi0@gmail.com>
2024-11-19 11:33:03 +0330

    chore(frontend): add image remotePatterns to next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "www.technolife.ir",
+                pathname: "**",
+            },
+            {
+                protocol: "https",
+                hostname: "technolife.ir",
+                pathname: "**",
+            },
+            {
+                protocol: "https",
+                hostname: "srvksnqjdyrlfxnvguri.supabase.co",
+                pathname: "**",
+            },
+            {
+                protocol: "http",
+                hostname: "localhost",
+                pathname: "**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
