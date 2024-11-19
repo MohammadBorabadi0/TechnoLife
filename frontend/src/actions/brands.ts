bde680aa2758fc98c48b3e45ab118177e0b5f24d
mohammadborabadi0.com <mohammadborabadi0@gmail.com>
2024-11-19 14:10:01 +0330

    feat(frontend): add server actions for brand CRUD operations (get, create, update, delete)

---
+++ frontend/src/actions/brands.ts
@@ -0,0 +1,147 @@
+"use server";
+
+import { uploadFileToSupabase } from "@/utils/functions";
+import { Brand } from "@/utils/types";
+import { revalidatePath } from "next/cache";
+
+const url = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export const getBrands = async (
+    page?: number,
+    limit?: number,
+    search?: string
+) => {
+    try {
+        const currentPage = page ? page : 1;
+        const currentLimit = limit ? limit : 10;
+        const searchQuery = search
+            ? `&search=${encodeURIComponent(search.trim())}`
+            : "";
+
+        const response = await fetch(
+            `${url}/brands?page=${currentPage}&limit=${currentLimit}${searchQuery}`,
+            { cache: "no-store" }
+        );
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getBrand = async (id: string) => {
+    try {
+        const response = await fetch(`${url}/brands/${id}`, {
+            cache: "no-store",
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const createBrand = async (formData: FormData) => {
+    try {
+        const { data: brands } = await getBrands();
+
+        const brandData = [];
+
+        const name = formData.get("name");
+        const files = formData.getAll("file");
+        const actives = formData.getAll("isActive");
+        const categories = formData.getAll("category");
+
+        if (brands.find((b: Brand) => b.name === name)) {
+            return {
+                success: false,
+                message: "برند با این نام از قبل وجود دارد.",
+            };
+        }
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const isActive = actives[i] === "true";
+            const category = categories[i];
+
+            // Upload File To Supabase And Get Url
+            const imageUrl = await uploadFileToSupabase(file as File, "brands");
+
+            brandData.push({
+                imageUrl,
+                isActive,
+                category,
+            });
+        }
+
+        const response = await fetch(`${url}/brands`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, categories: brandData }),
+        });
+        const data = await response.json();
+        console.log("DATA", data);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteBrand = async (id: string) => {
+    try {
+        const response = await fetch(`${url}/brands/${id}`, {
+            method: "DELETE",
+        });
+        const data = await response.json();
+        revalidatePath("/admin/brands");
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const updateBrand = async (formData: FormData, id: string) => {
+    try {
+        const { data: brands } = await getBrands();
+
+        const brandData = [];
+
+        const name = formData.get("name");
+        const files = formData.getAll("file");
+        const actives = formData.getAll("isActive");
+        const categories = formData.getAll("category");
+
+        if (brands.find((b: Brand) => b.name === name && b._id !== id)) {
+            return {
+                success: false,
+                message: "برند با این نام از قبل وجود دارد.",
+            };
+        }
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const isActive = actives[i] === "true";
+            const category = categories[i];
+
+            // Upload File To Supabase And Get Url
+            const imageUrl = await uploadFileToSupabase(file as File, "brands");
+
+            brandData.push({
+                imageUrl,
+                isActive,
+                category,
+            });
+        }
+
+        const response = await fetch(`${url}/brands/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, categories: brandData }),
+        });
+        const data = await response.json();
+        console.log("DATA", data);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
